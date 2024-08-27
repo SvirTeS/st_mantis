f5740e5139b676d971d72d86f2d869f34c7b95f9
SvirT <svirtes@gmail.com>
2024-08-27 16:21:46 +0400

    fix test

--- tests/test_manage_project.py
+++ tests/test_manage_project.py
@@ -13,7 +13,7 @@ project_data = Project(projectname=random_string('name', 10), description=random
 
 
 def test_add_project(app):
-    app.session.login('administrator', 'root')
+    app.session.ensure_login('administrator', 'root')
     old_list_project = app.project.get_project_list_from_ui()
     app.project.create(project_data)
     new_list_project = app.project.get_project_list_from_ui()
@@ -22,8 +22,7 @@ def test_add_project(app):
 
 
 def test_del_random_project_by_id(app):
-    if app.session.is_login_in_as('administrator') is False:
-        app.session.login('administrator', 'root')
+    app.session.ensure_login('administrator', 'root')
     if len(app.project.get_project_list_from_ui()) == 0:
         app.project.create(project_data)
     old_list_project = app.project.get_project_list_from_ui()
