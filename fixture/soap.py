1e5c441e894cf71d6158259a33b723d5dd263c50
svirt <svirtes@gmail.com>
2025-02-25 00:33:44 +0400

    /

--- fixture/soap.py
+++ fixture/soap.py
@@ -6,4 +6,9 @@ class SoapHelper:
         self.app = app
 
     def can_login(self, username, password):
-        client =
+        client = Client("http://localhost/mantisbt-1.2.20/api/soap/mantisconnect.php?wsdl")
+        try:
+            client.service.mc_login(username, password)
+            return True
+        except WebFault:
+            return False
