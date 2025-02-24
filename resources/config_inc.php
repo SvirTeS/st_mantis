9c2888d736fee0bd689dc9a6321bced29b8402a0
svirt <svirtes@gmail.com>
2025-02-25 00:23:47 +0400

    /

--- resources/config_inc.php
+++ resources/config_inc.php
@@ -5,3 +5,10 @@ $g_database_name = 'bugtracker';
 $g_db_username = 'root';
 $g_db_password = '';
 $g_use_captcha = 'off';
+$g_display_errors = array(
+  E_WARNING => 'none',
+  E_NOTICE => 'none',
+  E_USER_ERROR => 'halt',
+  E_USER_WARNING => 'none',
+  E_USER_NOTICE => 'none'
+);
